765bc50d4d9195d73dfa0e3346b63d2b1cdb7639
Marco <Marco@Marcos-iMac.fritz.box>
2014-12-26 22:54:38 +0100

    1

--- SaveAndLoad/ViewController.swift
+++ SaveAndLoad/ViewController.swift
@@ -8,18 +8,73 @@
 
 import UIKit
 
+// Documents Directory:    /Users/Marco/Library/Developer/CoreSimulator/Devices/65AC762C-00A1-4895-A9A9-E93D80F07B1A/data/Containers/Data/Application/DEB016A3-FA54-4584-90E0-F39ED7FBA334/Documents
+
+
+func documentsDirectory() -> String {
+
+let documentsFolderPath = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true)[0] as String
+
+     return documentsFolderPath
+}
+
+func fileInDocumentsDirectory(filename: String) -> String {
+    
+    return documentsDirectory().stringByAppendingPathComponent(filename)
+}
+
+
+//  File in Documents Directory:  /Users/Marco/Library/Developer/CoreSimulator/Devices/65AC762C-00A1-4895-A9A9-E93D80F07B1A/data/Containers/Data/Application/DEB016A3-FA54-4584-90E0-F39ED7FBA334/Documents/photo.jpg
+
+
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+        let documents = documentsDirectory()
+        println("Documents: \(documents)")
+        
+        let imagePath = fileInDocumentsDirectory("turkey.jpg")
+        println("Photo path: \(imagePath)")
+    
+            // Load image from resource bundle (read only)
+            var image = UIImage(named: "Funny-Turkey-Pictures-2014.jpg")
+            if image != nil {
+                // Save it to our documents folder
+                println(image)
+                
+                let result = saveImage(image!, path: imagePath)
+                println("saveImage: \(result)")
+            }
+        }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func saveImage(image: UIImage, path: String) -> Bool {
+        
+        let jpgImageData = UIImageJPEGRepresentation(image, 1.0)  // Oder UIImagePNGRepresentation
+    
+        let result = jpgImageData.writeToFile(path, atomically: true)
+    
+        return result
     }
-
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
